d168cde623a5b50fc164381a711602c4c2e257b2
Ben-97-0 <B.Norell@share.epsb.ca>
2024-11-28 14:23:34 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById("blessing").addEventListener("click", blessing);
+function blessing() {
+  alert("disguise!");
+}
+
+document.getElementById("dime").addEventListener("click", dime);
+function dime() {
+  alert("dozen!");
+}
+document.getElementById("late").addEventListener("click", late);
+function late() {
+  alert("Never!");
+}
+document.getElementById("pain").addEventListener("click", pain);
+function pain() {
+  alert("No gain!");
+}
+document.getElementById("time").addEventListener("click", time);
+function time() {
+  alert("when you are having fun!");
+}
